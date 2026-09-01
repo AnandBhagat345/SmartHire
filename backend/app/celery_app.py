9c6358ffedf4b23dd827f9e9fe3f48cd10a1cfd5
AnandBhagat345 <anandbhagat345@.com>
2026-09-01 16:47:56 +0530

    fix other things

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -4,15 +4,17 @@ import os
 
 load_dotenv()
 
-REDIS_URL = os.getenv("REDIS_URL")
+REDIS_URL = os.getenv(
+    "REDIS_URL",
+    "redis://localhost:6379"
+)
 
 celery_app = Celery(
     "smarthire",
     broker=f"{REDIS_URL}/1",
     backend=f"{REDIS_URL}/1",
-    include=[
+    include=
         "app.tasks.resume_tasks"
-    ]
 )
 
 celery_app.conf.update(
